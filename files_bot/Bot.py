f319a850eb7d49dff791b88a2cc1aa1089fe545c
Norman Ruiz <norman.ruiz@outlook.com.ar>
2023-08-03 13:53:54 -0300

    Agrego clase configuracion.

--- files_bot/Bot.py
+++ files_bot/Bot.py
@@ -1,6 +1,10 @@
+from files_bot.Configuracion import Configuracion
+
+
 class Bot:
     def __init__(self):
         self._estado = True
+        self._configuracion = None
 
     @property
     def estado(self):
@@ -10,5 +14,21 @@ class Bot:
     def estado(self, estado):
         self._estado = estado
 
+    @property
+    def configuracion(self):
+        return self._configuracion
+
+    @configuracion.setter
+    def configuracion(self, configuracion):
+        self._configuracion = configuracion
+
     def iniciar(self):
         print("Iniciando Bot's TerminalStatus...")
+        configuracion = Configuracion()
+        configuracion.cargar()
+        self.configuracion = configuracion
+        self.estado = self.configuracion.bot.estado
+        if not(self.estado):
+            print("Bot apagado por configuracion...")
+            return
+        print("Continuo la operacion...")
